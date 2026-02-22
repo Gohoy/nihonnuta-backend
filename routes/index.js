95861869289d3dc4f8ede0c7386311167ca30dbc
gaohongyu1 <gaohongyu1@xiaohongshu.com>
2026-02-22 21:50:39 +0800

    fix: remove migration controller reference from index route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const migrationController = require('../controllers/migration.controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ status: 'ok', service: 'nihonnuta-backend' });
 });
-
-/* 数据库迁移 */
-router.post('/migration/run', migrationController.runMigration);
 
 module.exports = router;
